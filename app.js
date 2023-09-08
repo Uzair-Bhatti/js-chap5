1f604516e6b4480762f707ce74a5d1230df56ccf
uzair bhatti <bhattiuzair775@gmail.com>
2023-09-08 19:23:51 +0500

    first

---
+++ app.js
@@ -0,0 +1,149 @@
+
+// Q1 //
+//var num1 = 3;
+//var num2 = 5;
+//var addition = num1 + num2;
+//document.write('The Sum Of Your Result is :' + addition)
+
+// Q2 //
+
+// subtraction
+//var num1 = 3;
+//var num2 = 5;
+//var subtraction = num1 - num2;
+//document.write('The Subtraction Of Your Result is :' + subtraction)
+
+// multiplication 
+//var num1 = 3;
+//var num2 = 5;
+//var multiplication = num1 * num2;
+//document.write('The Multiplication Of Your Result is :' + multiplication)
+
+// division
+//var num1 = 3;
+//var num2 = 5;
+//var division = num1 / num2;
+//document.write('The Division Of Your Result is :' + division)
+
+// Q3 //
+
+// a.declare a variable
+//var myVariable;
+
+// b.show the value after declaration
+//document.write("Value after variable declaration is:" + myVariable + "<br>")
+
+// c.initialize the variable with some number
+//myVariable = 5
+
+// d.show thw initialize value of the variable
+//document.write("Initial value: " + myVariable + "<br>")
+
+// e.increment the variable
+//myVariable++;
+
+// f.show the value of the variable after increment
+//document.write("Value after increment is: " + myVariable + "<br>")
+
+// g.add 7 to the variable
+//myVariable += 7;
+
+// h.show the value of the variable after addition
+//document.write("Value after addition is: " + myVariable + "<br>")
+
+// i.decrement the variable
+//myVariable--;
+
+// j.show thw value of the variable after decrement
+//document.write("Value after decrement is: " + myVariable + "<br>")
+
+// k.show the remainder after dividing the variable  value by 3
+//var remainder = myVariable % 3;
+
+// l.output the remainder
+//document.write("The remainder is: " + remainder)
+
+// Q4 //
+//var num1 = 600
+//var num2 = 5
+//var multiplication = num1 * num2
+//document.write("Total cost to buy 5 tickets to a movie is: " + multiplication)
+
+// Q5 //
+//var celsiusTemp = 25;
+//var farhrenhietTemp = (celsiusTemp * 9/5) + 32
+//document.write(celsiusTemp + "°C is " + farhrenhietTemp + "°F" + "<br>")
+//var farhrenhietTemp2 = 77;
+//var celsiusTemp2 = (farhrenhietTemp - 32) * 5/9;
+//document.write(farhrenhietTemp + "°F is " + celsiusTemp + "°C" + "<br>")
+
+// // Q6 //
+// var item1 = 650
+// var quantity1 = 3
+// var item2 = 100
+// var quantity2 =7
+// var total = (item1 * quantity1) + (item2 * quantity2)
+
+// document.write("Price of item 1 is " + item1 +"<br>")
+// document.write("Quantity of item 1 is " + quantity1 +"<br>")
+// document.write("Price of item 2 is " + item2 +"<br>")
+// document.write("Quantity of item 2 is " + quantity2 +"<br>")
+// document.write("Total cost of your order is " + total +"<br>")
+
+// // Q7//
+// var num1 = 980
+// var num2 = 804
+// var percentage =(num2/num1)*100
+
+// document.write("Total Marks : " + num1 +"<br>")
+// document.write("Marks Obtained : " + num2 +"<br>")
+// document.write("Percentage : " + percentage +"<br>")
+
+// // Q8 //
+// var PKR = 308.18
+// var $ = 10
+// var PKR2 = 82.16
+// var Riyal = 25
+
+// var PKRto$ = PKR * $
+// var PKRtoRiyal = PKR2 * Riyal
+// var total = PKRto$ + PKRtoRiyal
+
+// document.write("1 PKR to 10 $ is =" + PKRto$ + "<br>")
+// document.write("1 PKR to 10 Riyal is =" + PKRtoRiyal + "<br>")
+// document.write("Total currency=" + total + "<br>")
+
+// // Q9 // 
+// var num1 =16
+// var Result = (num1 + 5) * 10/2
+// document.write("total result is :" + Result)
+
+// // Q10 //
+// var current = 2023
+// var birth = 2005 
+// var age = current - birth
+// document.write("Current Year is =" + current + "<br>")
+// document.write("Birth Year is =" + birth + "<br>")
+// document.write("Your age is =" + age + "<br>")
+
+// // Q11 //
+// var radius = 20
+// var circumference = 2*3.142*radius
+// var area = 3.142 * (radius * radius)
+
+// document.write("The circumference is " + circumference +"<br>")
+// document.write("The area of circle is " + area )
+
+// // Q 12 //
+// var snack = "chocolate chip"; 
+// var current = 15
+// var maximum = 65 
+// var amount = 3 
+
+// var result = (maximum - current) * 365 * 2;
+
+// document.write("Favourite snack =" + snack + "<br>")
+// document.write("Current age =" + current + "<br>")
+// document.write("Maximum age =" + maximum + "<br>")
+// document.write("Amount of snack per day =" + snack + "<br>")
+// document.write("You will need "+ result + "chips to last you until the ripe old age of "+ maximum +  "<br>")
